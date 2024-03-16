fc484de42a4993d70bc3e44d3a9fd06b155fe6b3
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-16 13:47:10 +0530

    register function done.

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,12 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://127.0.0.1:27017/Instadatabase");
 
 const userSchema = mongoose.Schema({
-    name: String,
+    username: String,
     password: String,
-    gmail: String,
+    datecreated: {
+        type: Date,
+        default: Date.now()
+    }
 })
 
 module.exports= mongoose.model("User", userSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,38 @@ const express = require("express");
 const app = express();
 const body_parser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const UserModel = require("./index/User.model");
+
+const PORT = 8080 ||  process.env.PORT;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const express_session = require("express-session");
+
+app.use(session({
+    resave: false,
+    saveUninitiated: false,
+    secret: "secret",
+}));
+
+
+
+app.post("/register", function(req, res){
+    var userdata = new UserModel({
+        username: req.body.username,
+        password: req.body.password,
+    })
+
+    //or
+
+    // var {username, password} = req.body
+    // const userdata = new UserModel({username, password})
+})
+
+app.listen(PORT, () => {
+    console.log(`console is running on port: ${PORT}`);
+});
+
