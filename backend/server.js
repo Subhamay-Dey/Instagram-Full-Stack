300be89aa808926c4fb5a99bbd934cb4c8387314
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-16 14:16:11 +0530

    login function done.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const UserModel = require("./index/User.model");
+const bcrypt = require("bcrypt");
 
 const PORT = 8080 ||  process.env.PORT;
 
@@ -19,7 +20,29 @@ app.use(session({
     secret: "secret",
 }));
 
+app.post("/login", async function(req, res){
+    var {username, password} = req.body;
 
+    try{
+        const user = await UserModel.findOne({username});
+
+        if(!user){
+            res.status(404).json({message: "Username not found"});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, UserModel.password)
+
+        if(!isPasswordValid){
+            res.status(400).json({message: "password is incorrect"})
+        }
+
+        res.status(200).json({message: "Login successful"})
+    }
+    catch(error){
+        console.log(error);
+        res.status(501).json({message: "Server error"});
+    }
+})
 
 app.post("/register", function(req, res){
     var userdata = new UserModel({
