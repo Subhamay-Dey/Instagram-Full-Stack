95cfaebeec2c8b764c9f666b98b09d9015a3de5f
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-17 23:08:42 +0530

    sign up page rendered in the register route

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/login" element={<Login/>}/>
-        
+        <Route path="/register" element={<SignUp/>}/>
       </Routes>
     </>
   )
