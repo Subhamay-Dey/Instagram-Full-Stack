4bd60ca76813f865ce5c4720f0e0063968c0d728
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-17 23:07:32 +0530

    sidebar menu updated for the menubar in my insta clone

---
+++ frontend/client/src/components/SideMenuBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideMenuBar = () => {
+  return (
+    <>
+        <div className='w-[244px] h-screen bg-black'>
+            
+        </div>
+    </>
+  )
+}
+
+export default SideMenuBar
