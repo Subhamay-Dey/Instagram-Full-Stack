707b30facbfbebcc04da54f31655537903f57e1e
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-17 23:11:37 +0530

    post collection updated with a new property of date creation.

--- backend/posts.js
+++ backend/posts.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const postSchema = mongoose.Schema({
     id: String,
+    datecreated: {
+        type: Date,
+        default: Date.now()
+    }
 });
 
 const postsModel = mongoose.model("posts", postSchema);
