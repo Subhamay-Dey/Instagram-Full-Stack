7c60b3d77585d8dd92a40c83be007630fc0a055b
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-21 19:47:42 +0530

    made an array for the sidebar options and gave them their respective routes for routing.

--- frontend/client/src/components/SideMenuBar.jsx
+++ frontend/client/src/components/SideMenuBar.jsx
@@ -1,14 +1,88 @@
-import React from 'react'
-import instalogo from "../images/insta-logo1.jpg"
+import React from 'react';
+import instalogo1  from "../images/insta-logo1.jpg";
+import {useNavigate} from "react-router-dom";
+import { IoMdHome } from "react-icons/io";
+import { IoIosSearch } from "react-icons/io";
+import { MdOutlineExplore } from "react-icons/md";
+import { MdVideoLibrary } from "react-icons/md";
+
 
 const SideMenuBar = () => {
+
+  const navigate = useNavigate();
+
+  const menu = [
+    {
+      id: 1,
+      Name: "Home",
+      icon: {},
+      route: "/home",
+    },
+    {
+      id: 2,
+      Name: "Search",
+      icon: {},
+      route: "/search",
+    },
+    {
+      id: 3,
+      Name: "Explore",
+      icon: {},
+      route: "/explore",
+    },
+    {
+      id: 4,
+      Name: "Reels",
+      icon: {},
+      route: "/reels",
+    },
+    {
+      id: 5,
+      Name: "Messages",
+      icon: {},
+      route: "/messages",
+    },
+    {
+      id: 6,
+      Name: "Notifications",
+      icon: {},
+      route: "/notifications",
+    },
+    {
+      id: 7,
+      Name: "Create",
+      icon: {},
+      route: "/create",
+    },
+    {
+      id: 8,
+      Name: "Profile",
+      icon: {},
+      route: "/profile",
+    },
+    {
+      id: 9,
+      Name: "Threads",
+      icon: {},
+      route: "/threads",
+    },
+    {
+      id: 10,
+      Name: "More",
+      icon: {},
+      route: "/more",
+    }
+  ]
 
   return (
     <>
         <div className='w-[300px] h-screen bg-black'>
             <div>
-              <img src="instalogo" alt=""/>
+              <img src={instalogo1} alt="" width={200} className='pt-4 pl-2'/>
             </div>
+            
+
+            
         </div>
     </>
   )
