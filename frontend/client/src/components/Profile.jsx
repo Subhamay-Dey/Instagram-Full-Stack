1a2c5fe239569250ebb9e4cf53fe79c37d56cb1e
Subhamay-Dey <ncdey1966@gmail.com>
2024-03-23 23:59:46 +0530

    up

--- frontend/client/src/components/Profile.jsx
+++ frontend/client/src/components/Profile.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <>
-        <div className='w-[1278.4px] h-[840.8px] bg-black'></div>
+        <div className='w-[1278.4px] h-[840.8px] bg-'></div>
     </>
   )
 }
